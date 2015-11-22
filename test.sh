2e6cdc8a1a94343e616a180e382c4502f90a8376
Brian Gerstle <brian.gerstle@gmail.com>
2015-11-21 20:14:40 -0500

    update to 7.1 & add xcpretty

--- test.sh
+++ test.sh
@@ -1,3 +1,9 @@
 #! /bin/bash
 
-xcodebuild -scheme SwiftExample -project SwiftExample.xcodeproj -sdk iphonesimulator -destination 'platform=iOS Simulator,name=iPhone 6S,OS=9.0' test
+TEST_CMD="xcodebuild -scheme SwiftExample -project SwiftExample.xcodeproj -sdk iphonesimulator -destination 'platform=iOS Simulator,name=iPhone 6S,OS=9.1' build test"
+
+if [[ $TRAVIS ]]; then
+  eval "${TEST_CMD} | xcpretty"
+else
+  eval "$TEST_CMD"
+fi
